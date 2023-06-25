0d46b030cabae84e1beba508497be32045e2e545
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-26 06:26:42 +0800

    Add style rule for brush coloring and paintCell()

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function createRow(size){
     }
     return row;
 }
+
+function paintCell(e){
+    if(e.target.classList.contains("cell")){
+        e.target.style.backgroundColor = document.querySelector();
+    }
+
+    // 1.) if solid take value from color input directly (no global value)
+    // 2.) if random, no input needed just generate
+    // 3.) for ROYGBIV array color cycling (arr)
+    // .shift (delete and return color[0]) 
+    // .push (add back deleted to end of queue )
+    // 4.) Darkener and Lightener problem can be solved by using hsl
+}
