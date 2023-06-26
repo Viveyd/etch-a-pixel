eb6e715f1e46601ed5b8ceaa19454d64e72ea56a
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-26 22:41:49 +0800

    Add rgb to hsl functions

--- script.js
+++ script.js
@@ -56,8 +56,51 @@ function getRandomHSL(){
 }
 
 function rgbToHSL(rgb){
-    let trimmedRGB = rgb.slice(4, -1).split(",").map(num => Number(num));
-    const max = Math.max(trimmedRGB);
+    let trimmedRGB = rgb.slice(4, -1).split(",").map(num => Number(num)/255);
+    const max = trimmedRGB.indexOf(Math.max(trimmedRGB));
+    const min = trimmedRGB.indexOf(Math.min(trimmedRGB));
+    const chroma = trimmedRGB[max] - trimmedRGB[min];
+}
+
+function rgb2hsl(r, g, b) {
+    // see https://en.wikipedia.org/wiki/HSL_and_HSV#Formal_derivation
+    // convert r,g,b [0,255] range to [0,1]
+    r = r / 255,
+    g = g / 255,
+    b = b / 255;
+    // get the min and max of r,g,b
+    var max = Math.max(r, g, b);
+    var min = Math.min(r, g, b);
+    // lightness is the average of the largest and smallest color components
+    var lum = (max + min) / 2;
+    var hue;
+    var sat;
+    if (max == min) { // no saturation
+        hue = 0;
+        sat = 0;
+    } else {
+        var c = max - min; // chroma
+        // saturation is simply the chroma scaled to fill
+        // the interval [0, 1] for every combination of hue and lightness
+        sat = c / (1 - Math.abs(2 * lum - 1));
+        switch(max) {
+            case r:
+                // hue = (g - b) / c;
+                // hue = ((g - b) / c) % 6;
+                // hue = (g - b) / c + (g < b ? 6 : 0);
+                break;
+            case g:
+                hue = (b - r) / c + 2;
+                break;
+            case b:
+                hue = (r - g) / c + 4;
+                break;
+        }
+    }
+    hue = Math.round(hue * 60); // °
+    sat = Math.round(sat * 100); // %
+    lum = Math.round(lum * 100); // %
+    return [hue, sat, lum];
 }
 
 function hexToRGB(hex){
